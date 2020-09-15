4c7582888b28f3454638f34011c9ccdbeec534a5
tylerphopho <tyler.phopho@gmail.com>
2020-09-14 18:40:32 -0600

    Updates

---
+++ precache-manifest.90d66fe9e7b36525037854d49e6dac1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b326b58fe1101b1e0cfb5eaaa6fa2b9b",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "997374634e47b7b5d3a1",
+    "url": "/COVID-19-Tracker/static/css/main.7a9171af.chunk.css"
+  },
+  {
+    "revision": "8ef1e33bb2267a4d4455",
+    "url": "/COVID-19-Tracker/static/js/2.37559543.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/COVID-19-Tracker/static/js/2.37559543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997374634e47b7b5d3a1",
+    "url": "/COVID-19-Tracker/static/js/main.b5e2d76c.chunk.js"
+  },
+  {
+    "revision": "fe188a24c2a3117838f8",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.5bf882c0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
